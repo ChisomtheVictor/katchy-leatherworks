42f3700e6d3d7bd58392b196acfaa9d566cc1b9e
victornmeka <nme22001@byui.edu>
2024-08-24 11:54:07 +0100

    restyled and new features added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,99 +1,69 @@
-function toggleMenu() {
-    document.getElementById("nav-list").classList.toggle("open");
-    document.getElementById("hamburger-button").classList.toggle("open");
-}
+const hamburgerElement  = document.querySelector('#hamburgerButton');
+const navElement  = document.querySelector('.navList');
 
-const switchToX = document.getElementById("hamburger-button");
-switchToX.onclick = toggleMenu;
+hamburgerElement.addEventListener('click', ()=>{
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
 
 
 const todaysDate = new Date();
 
-const options = { year: 'numeric' }
+
+const options = {year: 'numeric'}
 document.getElementById('year').textContent = todaysDate.toLocaleDateString('en-us', options)
 const date = document.lastModified;
 document.getElementById('date').textContent = date;
 
 
+//cart scripts
+let iconCart = document.querySelector('.icon-cart');
+let cart = document.querySelector(".cart");
+let productCard = document.querySelector(".products");
+let close = document.querySelector(".close");
 
-//create an "url" variable
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad,US&units=imperial&id=524901&appid=5fe9959182c4e0e3b9a6e2414cd1f539";
-// select HTML elements in the document
-
-let WeatherIcon = document.querySelector('#weather-icon');
-let CurrentTemp = document.querySelector('#temp-value');
-let WeatherDesc = document.querySelector('#weather-desc');
-let WindSpeed = document.querySelector('#windspeed-value');
-let WindChillVal = document.querySelector('#windchill-value');
-
-async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            displayResults(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log();
+iconCart.addEventListener('click',()=>{
+    if(cart.style.right == '-100%'){
+        cart.style.right ='0';
+        productCard.style.transform ='translateX(-400px)';
+    }else{
+        cart.style.right ='-100%';
+        productCard.style.transform ='translateX(0)';
     }
-}
+})
+close.addEventListener('click', ()=> {
+    cart.style.right ='-100%';
+    productCard.style.transform ='translateX(0)';
+})
 
-apiFetch();
+//product
+let products = null;
+//get info from json file
+fetch('products.json')
+.then(response => response.json())
+.then(data => {
+    products = data;
+    addDataToHTML();
+})
 
-function displayResults(weatherData) {
+//list data in html
+function addDataToHTML(){
+    //remove default data in html
+    let listProductHTML = document.querySelector('.listProduct');
+    listProductHTML.innerHTML = '';
 
-    {
-        CurrentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-        const windSpeed = weatherData.wind.speed;
-        const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-        const desc = weatherData.weather[0].description;
-
-        WeatherIcon.setAttribute('src', iconsrc);
-        WeatherIcon.setAttribute('alt', desc);
-        WeatherDesc.textContent = desc;
-
-        WindSpeed.innerHTML = `${windSpeed.toFixed(2)}`;
-
-        if (windSpeed < 3 || weatherData.main.temp < 20) {
-
-            WindChillVal.textContent = "N/A";
-        } else {
-            let windChill = 35.74 + 0.6215 * weatherData.main.temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * weatherData.main.temp * Math.pow(windSpeed, 0.16);
-            WindChillVal.textContent = windChill.toFixed(2) + "°F";
-        }
-    }
-
-}
-
-//LazyLoad Script
-const images = document.querySelectorAll("img[data-src]");
-
-const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src");
-    };
-};
-
-
-
-if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-
-            }
+    //add new info
+    if(products!= null){
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item');
+            newProduct.innerHTML =
+            `<img src=${product.image} alt="Product Image">
+            <h2>${product.name}</h2>
+            <div class="price">${product.price}</div>
+            <button onclick= "addCart(${product.id})">Add to Cart</button>`;
+            listProductHTML.appendChild(newProduct);
         });
-    });
-    images.forEach((img) => {
-        observer.observe(img);
-    });
-} else {
-    images.forEach((img) => {
-        loadImages(img);
-    });
+    }
+
 }
